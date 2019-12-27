6ada4b7725ec6b72000c7eb90370feca55df3166
Axel Redois <axelredois25@gmail.com>
2019-12-27 23:05:23 +0100

    add image and update readme

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Dynamic Text to Markdown translater
+
+![]('public/markdown-app.png')
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { sampleText } from './sampleText'
+import marked from 'marked'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      text: sampleText
+    }
+  }
+
+  componentDidMount () {
+    const text = localStorage.getItem('text')
+  
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText})
+    }
+  }
+
+  componentDidUpdate () {
+    localStorage.setItem('text', this.state.text)
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderMarkdown = content => {
+    const __html = marked(content, {sanitize:true})
+    return { __html }
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+            className="form-control" 
+            rows="35" 
+            value={this.state.text}
+            onChange={this.handleChange}
+            />
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderMarkdown(this.state.text)}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
